4bbd63e4704511192313650e6808dae5bf03aa4d
2012Inga <inganathi2001@gmail.com>
2023-11-01 04:16:48 -0700

    getenv_updated

--- test/getenv.c
+++ test/getenv.c
@@ -3,30 +3,28 @@
 #include <string.h>
 #include <unistd.h>
 
- extern char **environ; 
+extern char **environ;
+
 char *_getenv(const char *name) {
-
-  char **env = environ;
- 
-  while (*env) {
-    char *sepa = strchr(*env, '=');
-    if (sepa) {
-      if (strncmp(*env, name, sepa - *env) == 0) {
-        return sepa + 1;
-      }
+    if (name == NULL) {
+        return NULL; /* Invalid input */
     }
-    env++;
-  }
-   return NULL;
-}
-int main(int argc, char *argv[]) {
-  char *name = argv[1];
-   char *value = _getenv(name);
-   if (value) {
-    printf("%s: %s\n", name, value);
-  } else {
-     printf("The environment variable %s is not defined.\n", name);
-  }
 
-  return 0;
+    for (char **env = environ; *env != NULL; env++) {
+        char *variable = *env;
+        if (strncmp(variable, name, strlen(name)) == 0 && variable[strlen(name)] == '=') {
+            return variable + strlen(name) + 1;
+        }
+    }
+    return NULL; /* Variable not found */
+}
+
+int main() {
+    char *path = _getenv("PATH");
+    if (path != NULL) {
+        printf("PATH = %s\n", path);
+    } else {
+        printf("PATH environment variable not found.\n");
+    }
+    return 0;
 }
