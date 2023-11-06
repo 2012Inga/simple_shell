834a715a97f221f4e11b2880939ad4f5683351d0
chingling008 <chinglingmadness@gmail.com>
2023-11-06 05:11:59 -0800

    It's just needs betty it complies

--- test1/basic_shell.c
+++ test1/basic_shell.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-int main(int argc, char **argv, char **env) {
-    char *command = NULL;
+extern char **environ;
+
+int main(void) {
+char *command = NULL;
     size_t n_char = 0;
     char *emp[] = {NULL, NULL};
-    int status;
+    int status ,i;
     bool oneline = isatty(STDIN_FILENO); 
     pid_t child;
 
@@ -19,7 +21,7 @@ int main(int argc, char **argv, char **env) {
             break;
         }
 
-        int i = 0;
+        i = 0;
         while (command[i]) {
             if (command[i] == '\n') {
                 command[i] = 0;
@@ -32,7 +34,7 @@ int main(int argc, char **argv, char **env) {
         if (child < 0) {
             printf("fork failed");
         } else if (child == 0) {
-            if (execve(emp[0], emp, env) == -1) {
+            if (execve(emp[0], emp, environ) == -1) {
                 printf("%s: No such file or directory\n", emp[0]);
                 exit(1);
             }
