4f21ed09151fd07b525d16565f0e1541e6bf26b6
2012Inga <inganathi2001@gmail.com>
2023-11-07 01:58:50 -0800

    basic_shell can handle basic-commands

--- test1/basic_shell.c
+++ test1/basic_shell.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 /**
  * main - Entry point for the custom shell program.
  *
@@ -14,76 +14,71 @@
  *
  * Return: Always returns 0 to indicate successful execution.
  */
-int main(void)
-{
-	char *command = NULL;
-	size_t n_char = 0;
-	char *emp[] = {NULL, NULL};
-	int status, i;
-	bool oneline = isatty(STDIN_FILENO);
-	pid_t child;
 
-	while (1)
-	{
-		if (oneline)
-		{
-			printf("#shell27$ ");
-		}
-		if (getline(&command, &n_char, stdin) == EOF)
-		{
-			if (oneline)
-			{
-				printf("\n");
-			}
-			break;
-		}
+int main(void) {
+    char *command = NULL;
+    size_t n_char = 0;
+    int status, i;
+    bool oneline = isatty(STDIN_FILENO);
+    pid_t child;
 
-		/* Parse the command and arguments */
-		char *token;
-		char *argv[64]; /* Maximum number of arguments */
+    while (1) {
+        if (oneline) {
+            printf("#shell27$ ");
+        }
+        if (getline(&command, &n_char, stdin) == EOF) {
+            if (oneline) {
+                printf("\n");
+            }
+            break;
+        }
 
-		int argc = 0;
+        /* Parse the command and arguments */
+        char *token;
+        char *argv[64]; /* Maximum number of arguments */
 
-		token = strtok(command, "\n" /* Tokenize by space and newline*/
-		while (token != NULL)
-		{
-			argv[argc] = token;
-			argc++;
-			token = strtok(NULL, "\n");
-		}
+        int argc = 0;
 
-		argv[argc] = NULL; /* NULL - terminate the argument array*/
+        token = strtok(command, " \n"); /* Tokenize by space and newline */
+        while (token != NULL) {
+            argv[argc] = token;
+            argc++;
+            token = strtok(NULL, " \n");
+        }
 
-		child = fork();
-		if (child < 0)
-		{
-			printf("fork failed");\
-		}
-		else if (child == 0)
-		{
+        argv[argc] = NULL; /* NULL-terminate the argument array */
 
-		i = 0;
-		while (command[i])
-		{
+        /* Iterate through directories in PATH */
+        char *path = getenv("PATH");
+        char *path_copy = strdup(path);
+        char *dir = strtok(path_copy, ":");
 
-		emp[0] = strdup(command);
-		child = fork()
-			printf("fork failed");
-		}
-		else if (child == 0)
-		{
-			if (execve(emp[0], emp, environ) == -1)
-			{
-				printf("%s: Command not found\n", emp[0]);
-				exit(1);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
-	}
+        while (dir != NULL) {
+            char full_path[1024]; /* Adjust the size as needed */
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir, argv[0]);
 
-	free(command);
-	return (0);
+            /* Check if the command exists in the directory */
+            if (access(full_path, X_OK) == 0) {
+                child = fork();
+                if (child < 0) {
+                    printf("fork failed\n");
+                } else if (child == 0) {
+                    if (execvp(full_path, argv) == -1) {
+                        printf("%s: Command execution error\n", argv[0]);
+                        exit(1);
+                    }
+                } else {
+                    wait(&status);
+                }
+                break;
+            }
+
+            dir = strtok(NULL, ":");
+        }
+
+        free(path_copy);
+    }
+
+    free(command);
+    return 0;
 }
--- test1/main.h
+++ test1/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-	
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdbool.h>
@@ -12,5 +12,5 @@
 #include <sys/types.h>
 
 /* Basic_shell */
-int main(void);  
+int main(void);
 #endif
