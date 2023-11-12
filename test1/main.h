c4f971719c39efd65bf71a61997d437fae4f8218
2012Inga <inganathi2001@gmail.com>
2023-11-11 21:47:48 -0800

    builtin_command-added_to_handle_setenv_and_unsetenv

--- test1/main.h
+++ test1/main.h
@@ -15,16 +15,59 @@ extern char **environ;
 
 #define BUFFER_SIZE 1024
 
-/* Get line function */
+/* Function to read a line from standard input */
 char *_getline(void);
 
+/**
+ * Split a string into an array of tokens based on a delimiter.
+ *
+ * @param input The input string to be split.
+ * @param delimiter The delimiter used for splitting the string.
+ * @param count Pointer to an integer to store the number of tokens.
+ * @return An array of strings (tokens).
+ */
 char **splitString(const char *input, const char *delimiter, int *count);
+
+/**
+ * Custom implementation of strtok function.
+ *
+ * @param str The string to tokenize.
+ * @param delim The delimiter character.
+ * @return A pointer to the next token, or NULL if there are no more tokens.
+ */
 char *strtok_custom(char *str, const char *delim);
 
+/**
+ * Execute a shell command.
+ *
+ * This function takes a command string as input, tokenizes it into arguments,
+ * and attempts to execute the command string using execv. If the command execution
+ * fails, it iterates through directories in the PATH environment variable to
+ * find and execute the command. If the command is "env," it prints the environment variables.
+ *
+ * @param command The command string to be executed.
+ */
 void execute_command(char *command);
 
+/**
+ * Set or modify an environment variable.
+ * Command syntax: setenv VARIABLE VALUE
+ * Prints an error message on stderr on failure.
+ *
+ * @param variable The name of the environment variable.
+ * @param value The value to set for the environment variable.
+ * @return 0 on success, -1 on failure.
+ */
+int set_env_variable(const char *variable, const char *value);
 
+/**
+ * Unset an environment variable.
+ * Command syntax: unsetenv VARIABLE
+ * Prints an error message on stderr on failure.
+ *
+ * @param variable The name of the environment variable to unset.
+ * @return 0 on success, -1 on failure.
+ */
+int unset_env_variable(const char *variable);
 
-/* Basic_shell */
-
-#endif
+#endif /* MAIN_H */
