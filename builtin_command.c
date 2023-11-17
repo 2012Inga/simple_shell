60aa0037b4129d917e419d4b8feac65ead3858a5
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 19:39:07 +0200

    betty plus

--- builtin_command.c
+++ builtin_command.c
@@ -6,82 +6,52 @@
  * @value: The value to set for the environment variable.
  *
  * Return: 0 on success, -1 on failure.
- */ 
-  int
-set_env_variable (const char *variable, const char *value) 
+ */
+int set_env_variable(const char *variable, const char *value)
 {
-  
-if (setenv (variable, value, 1) == -1)
-    
-    {
-      
-fprintf (stderr, "Error: Could not set environment variable %s\n",
-		variable);
-      
-return (-1);
-    
-}
-  
-return (0);
+	if (setenv(variable, value, 1) == -1)
+	{
+		fprintf(stderr, "Error: Could not set environment variable %s\n", variable);
+		return (-1);
+	}
 
+	return (0);
 }
 
-
- 
 /**
  * unset_env_variable - Unset an environment variable.
  * @variable: The name of the environment variable to unset.
  *
  * Return: 0 on success, -1 on failure.
- */ 
-  int
-unset_env_variable (const char *variable) 
+ */
+int unset_env_variable(const char *variable)
 {
-  
-if (unsetenv (variable) == -1)
-    
-    {
-      
-fprintf (stderr, "Error: Could not unset environment variable %s\n",
-		variable);
-      
-return (-1);
-    
-}
-  
-return (0);
+	if (unsetenv(variable) == -1)
+	{
+		fprintf(stderr, "Error: Could not unset environment variable %s\n", variable);
+		return (-1);
+	}
 
+	return (0);
 }
 
-
-void
-exit_arg (char *arg)
+void exit_arg(char *arg)
 {
-  
-int status;
-  
- 
-if (arg == NULL)
-    {
-      
- 
-status = 0;
-    
-}
-  else
-    {
-      
- 
-status = atoi (arg);
-    
-}
-  
-exit (status);
+	int status;
 
+	if (arg == NULL)
+	{
+		status = 0;
+	}
+	else
+	{
+		status = atoi(arg);
+	}
+
+	exit(status);
 }
 
 /**
  * execute_command - Execute a shell command.
  * @command: The command string to be executed.
  */
-
