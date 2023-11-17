99460834c1eda970d62f55c90125c16e992875ad
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 13:44:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main(void) {
     bool oneline = isatty(STDIN_FILENO);
     pid_t child;
     char *arg;
+    char *env_var;
+    char *variable;
+    char *value;
 
     while (1) {
         if (oneline) {
@@ -36,6 +39,14 @@ int main(void) {
                     exit(EXIT_SUCCESS);
                 } else {
                     wait(NULL);
+                    env_var = strtok(command, " \t");
+                    if (strncmp(env_var, "export", 6) == 0) {
+                        variable = strtok(NULL, " \t");
+                        value = strtok(NULL, " \t");
+                        set_env_variable(variable, value);
+                    } else if (strncmp(env_var, "unset", 5) == 0) {
+                        variable = strtok(NULL, " \t");
+                        unset_env_variable(variable);
                 }
             }
         }
