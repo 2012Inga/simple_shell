d43fa5a2cfcd7b04a07ffa42e9302709f27093e2
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 12:26:31 +0200

    Lets handle this one

--- handle_separator.c
+++ handle_separator.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+
+
 /**
  * execute_single_command - Execute a single command in a child process.
  * @command: The command to execute.
@@ -24,82 +26,20 @@ void execute_single_command(char *command) {
 }
 
 /**
- * execute_logical_operator - Execute a command based on the result of the previous one.
- * @command: The command to execute.
- * @prev_status: The exit status of the previous command.
- * @logical_operator: The logical operator (&& or || or ;).
- * Return: The exit status of the executed command.
- */
-int execute_logical_operator(char *command, int prev_status, char *logical_operator) {
-    int status;
-    pid_t child;
-
-    if (strcmp(logical_operator, ";") == 0) {
-        /* Always execute the command separated by ; */
-        child = fork();
-        if (child == -1) {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (child == 0) {
-            /* Child process */
-            execute_command(command);
-            exit(EXIT_SUCCESS);
-        } else {
-            /* Parent process */
-            waitpid(child, &status, 0);
-            return status;
-        }
-    }
-
-    /* Execute the command based on the logical operator */
-    child = fork();
-    if (child == -1) {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (child == 0) {
-        /* Child process */
-        execute_command(command);
-        exit(EXIT_SUCCESS);
-    } else {
-        /* Parent process */
-        waitpid(child, &status, 0);
-
-        if ((strcmp(logical_operator, "&&") == 0 && status == 0) ||
-            (strcmp(logical_operator, "||") == 0 && status != 0)) {
-            return status;
-        } else {
-            return prev_status;
-        }
-    }
-}
-
-/**
  * handle_separator - Execute commands separated by logical operators && or || or ;.
  * @argv: An array of command-line arguments.
  * @argc: The number of command-line arguments.
  * Return: The exit status of the last executed command.
  */
 int handle_separator(char *argv[], int argc) {
-    int status = 0,i;
+    int status = 0, i;
 
     for (i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0 || strcmp(argv[i], ";") == 0) {
-            /* Logical operator found */
-            if (i + 1 < argc) {
-                /* Execute command based on the logical operator */
-                status = execute_logical_operator(argv[i + 1], status, argv[i]);
-                i++; /* Skip the next argument */
-            } else {
-                /* Error: Logical operator without a following command */
-                fprintf(stderr, "Syntax error: Logical operator without a following command\n");
-                return -1;
-            }
+        if (strpbrk(argv[i], "&;|")) {
+            
+            printf("Skipping command with separators: %s\n", argv[i]);
         } else {
-            /* Single command */
+            
             execute_single_command(argv[i]);
         }
     }
