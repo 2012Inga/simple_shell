d208aff9b282314b6e88abb4b1cf90162989ccfd
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 11:56:54 +0200

    Update handle_separator.c

--- handle_separator.c
+++ handle_separator.c
@@ -5,27 +5,22 @@
  * @command: The command to execute.
  * Return: void
  */
-void execute_single_command(char *command)
-{
-	pid_t child;
+void execute_single_command(char *command) {
+    pid_t child = fork();
 
-	child = fork();
-	if (child == -1)
-	{
-		perror("fork failed");
-		exit(EXIT_FAILURE);
-	}
+    if (child == -1) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
 
-	if (child == 0)
-	{
-		/* Child process */
-		execute_command(command);
-		exit(EXIT_SUCCESS);
-	} else
-	{
-		/* Parent process */
-		waitpid(child, NULL, 0);
-	}
+    if (child == 0) {
+        /* Child process */
+        execute_command(command);
+        exit(EXIT_SUCCESS);
+    } else {
+        /* Parent process */
+        waitpid(child, NULL, 0);
+    }
 }
 
 /**
@@ -35,38 +30,51 @@ void execute_single_command(char *command)
  * @logical_operator: The logical operator (&& or || or ;).
  * Return: The exit status of the executed command.
  */
-int execute_logical_operator(char *command, int prev_status, char *logical_operator)
-{
-	int status;
-	pid_t child;
+int execute_logical_operator(char *command, int prev_status, char *logical_operator) {
+    int status;
+    pid_t child;
 
-	if ((strcmp(logical_operator, "&&") == 0 && prev_status == 0) ||
-	    (strcmp(logical_operator, "||") == 0 && prev_status != 0) ||
-	    (strcmp(logical_operator, ";") == 0))
-	    {
-		/* Execute the command only if the condition is met */
-		child = fork();
-		if (child == -1)
-		{
-			perror("fork failed");
-			exit(EXIT_FAILURE);
-		}
+    if (strcmp(logical_operator, ";") == 0) {
+        /* Always execute the command separated by ; */
+        child = fork();
+        if (child == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
 
-		if (child == 0)
-		{
-			/* Child process */
-			execute_command(command);
-			exit(EXIT_SUCCESS);
-		} else
-		{
-			/* Parent process */
-			waitpid(child, &status, 0);
-			return (status);
-		}
-	}
+        if (child == 0) {
+            /* Child process */
+            execute_command(command);
+            exit(EXIT_SUCCESS);
+        } else {
+            /* Parent process */
+            waitpid(child, &status, 0);
+            return status;
+        }
+    }
 
-	/* Return the previous status if the condition is not met */
-	return (prev_status);
+    /* Execute the command based on the logical operator */
+    child = fork();
+    if (child == -1) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child == 0) {
+        /* Child process */
+        execute_command(command);
+        exit(EXIT_SUCCESS);
+    } else {
+        /* Parent process */
+        waitpid(child, &status, 0);
+
+        if ((strcmp(logical_operator, "&&") == 0 && status == 0) ||
+            (strcmp(logical_operator, "||") == 0 && status != 0)) {
+            return status;
+        } else {
+            return prev_status;
+        }
+    }
 }
 
 /**
@@ -75,33 +83,26 @@ int execute_logical_operator(char *command, int prev_status, char *logical_opera
  * @argc: The number of command-line arguments.
  * Return: The exit status of the last executed command.
  */
-int handle_separator(char *argv[], int argc)
-{
-	int status = 0;
-	int i;
+int handle_separator(char *argv[], int argc) {
+    int status = 0;
 
-	for (i = 0; i < argc; i++)
-	{
-		if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0)
-		{
-			/* Logical operator found */
-			if (i + 1 < argc)
-			{
-				/* Execute command based on the logical operator */
-				status = execute_logical_operator(argv[i + 1], status, argv[i]);
-				i++; /* Skip the next argument */
-			} else
-			{
-				/* Error: Logical operator without a following command */
-				fprintf(stderr, "Syntax error: Logical operator without a following command\n");
-				return (-1);
-			}
-		} else
-		{
-			/* Single command */
-			execute_single_command(argv[i]);
-		}
-	}
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0 || strcmp(argv[i], ";") == 0) {
+            /* Logical operator found */
+            if (i + 1 < argc) {
+                /* Execute command based on the logical operator */
+                status = execute_logical_operator(argv[i + 1], status, argv[i]);
+                i++; /* Skip the next argument */
+            } else {
+                /* Error: Logical operator without a following command */
+                fprintf(stderr, "Syntax error: Logical operator without a following command\n");
+                return -1;
+            }
+        } else {
+            /* Single command */
+            execute_single_command(argv[i]);
+        }
+    }
 
-	return (status);
+    return status;
 }
