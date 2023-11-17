4d5ee26f503eaa5a58a7ca8247b2c0eb15255f04
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 14:20:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
-
 int main(void) {
     char *command = NULL;
-    char *arg = NULL;
-    char *env_var = NULL;
-    char *variable;
-    char *value;
-    char *command_copy;
     bool oneline = isatty(STDIN_FILENO);
     pid_t child;
+    char *arg;
+    char *env_var;
+    char *variable;
+    char *value;
 
     while (1) {
         if (oneline) {
@@ -24,42 +22,27 @@ int main(void) {
             break;
         }
 
-        command_copy = strdup(command);
-        env_var = strtok(command_copy, " \t");
-        if (command_copy == NULL) {
-            perror("strdup failed");
-            exit(EXIT_FAILURE);
-        }
-        if (strncmp(env_var, "setenv", 6) == 0) {
-            variable = strtok(NULL, " \t");
-            value = strtok(NULL, " \t");
-            set_env_variable(variable, value);
-        } else if (strncmp(env_var, "unsetenv", 8) == 0) {
-            variable = strtok(NULL, " \t");
-            unset_env_variable(variable);
+        if (strncmp(command, "exit", 4) == 0) {
+            arg = strtok(command + 4, " \t");
+            exit_arg(arg);
         } else {
-            if (strncmp(command, "exit", 4) == 0) {
-                arg = strtok(command + 4, " \t");
-                exit_arg(arg);
+            if (strncmp(command, "cd", 2) == 0) {
+                _buildInCmd(command);
             } else {
-                if (strncmp(command, "cd", 2) == 0) {
-                    _buildInCmd(command);
-                } else {
-                    if ((child = fork()) == -1) {
-                        perror("fork failed");
-                        exit(EXIT_FAILURE);
-                    }
+                if ((child = fork()) == -1) {
+                    perror("fork failed");
+                    exit(EXIT_FAILURE);
+                }
 
-                    if (child == 0) {
-                        execute_command(command);
-                        exit(EXIT_SUCCESS);
-                    } else {
-                        wait(NULL);
-                    }
+                if (child == 0) {
+                    execute_command(command);
+                    exit(EXIT_SUCCESS);
+                } else {
+                    wait(NULL);
                 }
             }
         }
-        free(command_copy);
+
         free(command);
     }
 
