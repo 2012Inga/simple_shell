e7f514abdde9463165924d638b1671e88b7c403e
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 20:25:36 +0200

    Update strtok.c

--- strtok.c
+++ strtok.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-/* Prototype for strtok_custom */
-char *strtok_custom(char *str, const char *delim);
-
 /**
  * splitString - Split a string into an array of tokens based on a delimiter.
  * @input: The input string to be split.
@@ -11,54 +8,81 @@ char *strtok_custom(char *str, const char *delim);
  *
  * Return: An array of strings (tokens).
  */
-char **splitString(const char *input, const char *delimiter, int *count)
+char **
+splitString (const char *input, const char *delimiter, int *count)
 {
-    int tokenCount = 0;
-    char **tokens = NULL;
-    int maxTokens = 10; /* Initial array size, can be adjusted based on your needs */
-    char *token;
-    char *str;
-    *count = 0;
 
-    tokens = (char **)malloc(maxTokens * sizeof(char *));
-    if (tokens == NULL)
+  int tokenCount = 0;
+
+  char **tokens = NULL;
+
+  int maxTokens
+      = 10; /* Initial array size, can be adjusted based on your needs */
+
+  char *token;
+
+  char *str;
+
+  *count = 0;
+
+  tokens = (char **)malloc (maxTokens * sizeof (char *));
+
+  if (tokens == NULL)
+
     {
-        return NULL; /* Memory allocation error */
+
+      return NULL; /* Memory allocation error */
     }
 
-    str = strdup(input);
-    if (str == NULL)
+  str = strdup (input);
+
+  if (str == NULL)
+
     {
-        free(tokens);
-        return NULL; /* Memory allocation error */
+
+      free (tokens);
+
+      return NULL; /* Memory allocation error */
     }
 
-    token = strtok_custom(str, delimiter);
-    while (token != NULL)
-    {
-        tokens[tokenCount] = strdup(token);
-        tokenCount++;
+  token = strtok_custom (str, delimiter);
 
-        if (tokenCount >= maxTokens)
+  while (token != NULL)
+
+    {
+
+      tokens[tokenCount] = strdup (token);
+
+      tokenCount++;
+
+      if (tokenCount >= maxTokens)
+
         {
-            maxTokens *= 2;
-            tokens = (char **)realloc(tokens, maxTokens * sizeof(char *));
-            if (tokens == NULL)
+
+          maxTokens *= 2;
+
+          tokens = (char **)realloc (tokens, maxTokens * sizeof (char *));
+
+          if (tokens == NULL)
+
             {
-                free(str);
-                return NULL; /* Memory allocation error */
+
+              free (str);
+
+              return NULL; /* Memory allocation error */
             }
         }
 
-        token = strtok_custom(NULL, delimiter);
+      token = strtok_custom (NULL, delimiter);
     }
 
-    free(str);
-    tokens[tokenCount] = NULL; /* Null-terminate the array */
+  free (str);
 
-    *count = tokenCount;
+  tokens[tokenCount] = NULL; /* Null-terminate the array */
 
-    return tokens;
+  *count = tokenCount;
+
+  return tokens;
 }
 
 /**
@@ -68,46 +92,69 @@ char **splitString(const char *input, const char *delimiter, int *count)
  *
  * Return: A pointer to the next token, or NULL if there are no more tokens.
  */
-char *strtok_custom(char *str, const char *delim)
+char *
+strtok_custom (char *str, const char *delim)
 {
-    static char *lastToken = NULL; /* Stores the last token between calls */
-    char *token;
-    char *strPointer;
 
-    if (str != NULL)
+  static char *lastToken = NULL; /* Stores the last token between calls */
+
+  char *token;
+
+  char *strPointer;
+
+  if (str != NULL)
+
     {
-        lastToken = str;
+
+      lastToken = str;
     }
-    else if (lastToken == NULL)
+
+  else if (lastToken == NULL)
+
     {
-        return NULL;
+
+      return NULL;
     }
 
-    token = lastToken;
+  token = lastToken;
 
-    while (*lastToken != '\0')
+  while (*lastToken != '\0')
+
     {
-        if (strchr(delim, *lastToken) != NULL)
+
+      if (strchr (delim, *lastToken) != NULL)
+
         {
-            *lastToken = '\0';
-            lastToken++;
-            return token;
+
+          *lastToken = '\0';
+
+          lastToken++;
+
+          return token;
         }
-        lastToken++;
+
+      lastToken++;
     }
 
-    strPointer = token;
+  strPointer = token;
 
-    while (*strPointer != '\0')
+  while (*strPointer != '\0')
+
     {
-        if (strchr(delim, *strPointer) != NULL)
+
+      if (strchr (delim, *strPointer) != NULL)
+
         {
-            *strPointer = '\0';
-            lastToken = strPointer + 1;
-            return token;
+
+          *strPointer = '\0';
+
+          lastToken = strPointer + 1;
+
+          return token;
         }
-        strPointer++;
+
+      strPointer++;
     }
 
-    return token;
+  return token;
 }
