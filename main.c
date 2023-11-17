d24bfe21343efa5f1d61e0c5e5699e0e2a4a555e
chingling008 <130581550+chingling008@users.noreply.github.com>
2023-11-17 13:51:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
+
 int main(void) {
     char *command = NULL;
-    bool oneline = isatty(STDIN_FILENO);
-    pid_t child;
-    char *arg;
-    char *env_var;
+    char *arg = NULL;
+    char *env_var = NULL;
     char *variable;
     char *value;
+    char *variable;
+    bool oneline = isatty(STDIN_FILENO);
+    pid_t child;
 
     while (1) {
         if (oneline) {
@@ -22,31 +24,33 @@ int main(void) {
             break;
         }
 
-        if (strncmp(command, "exit", 4) == 0) {
-            arg = strtok(command + 4, " \t");
-            exit_arg(arg);
+        env_var = strtok(command, " \t");
+        if (strncmp(env_var, "export", 6) == 0) {
+            variable = strtok(NULL, " \t");
+            value = strtok(NULL, " \t");
+            set_env_variable(variable, value);
+        } else if (strncmp(env_var, "unset", 5) == 0) {
+            variable = strtok(NULL, " \t");
+            unset_env_variable(variable);
         } else {
-            if (strncmp(command, "cd", 2) == 0) {
-                _buildInCmd(command);
+            if (strncmp(command, "exit", 4) == 0) {
+                arg = strtok(command + 4, " \t");
+                exit_arg(arg);
             } else {
-                if ((child = fork()) == -1) {
-                    perror("fork failed");
-                    exit(EXIT_FAILURE);
-                }
-
-                if (child == 0) {
-                    execute_command(command);
-                    exit(EXIT_SUCCESS);
+                if (strncmp(command, "cd", 2) == 0) {
+                    _buildInCmd(command);
                 } else {
-                    wait(NULL);
-                    env_var = strtok(command, " \t");
-                    if (strncmp(env_var, "export", 6) == 0) {
-                        variable = strtok(NULL, " \t");
-                        value = strtok(NULL, " \t");
-                        set_env_variable(variable, value);
-                    } else if (strncmp(env_var, "unset", 5) == 0) {
-                        variable = strtok(NULL, " \t");
-                        unset_env_variable(variable);
+                    if ((child = fork()) == -1) {
+                        perror("fork failed");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    if (child == 0) {
+                        execute_command(command);
+                        exit(EXIT_SUCCESS);
+                    } else {
+                        wait(NULL);
+                    }
                 }
             }
         }
